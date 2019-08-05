388c9a1239e27af6577d2521f93523c9b5e84c20
shabudin <52714307+shabudin@users.noreply.github.com>
2019-08-04 21:01:53 -0400

    Add files via upload

---
+++ assets/data/myinfo.ts
@@ -0,0 +1,9 @@
+import { Shabudan } from 'src/app/shabudan';
+
+export const MYINFO: Shabudan = {
+    id: 991511959,
+    name: "Andy Shabudin",
+    username: "shabudan",
+    campus: "Davis",
+    atitle: "Angular More"
+};
